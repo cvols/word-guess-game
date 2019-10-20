5f14492e0f80708f44db35e7d44dd0d17d75d4ea
boz7 <zbratkovich@gmail.com>
2019-10-20 14:32:41 -0400

    adding variables to randomize words and text sections to be modified in the HTML

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
 let library = ['phaser', 'transporter', 'starship', 'federation', 'data', 'borg', 'klingon'];
+let user_guesses = [];
+let word_2_guess = library[Math.floor(Math.random() * library.length)];
+
+// getting HTML elements to modify
+let display_guess_word = document.getElementById("guess_word");
+let wrong_guesses = document.getElementById("letters_guessed");
+
+// setting challenge word to be dashes
